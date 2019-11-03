2b93e24ed715358ac714b66c6935701156fd2877
volynvlad <volynvlad@gmail.com>
2019-11-04 01:17:13 +0300

    added tests and metrics

--- hash_table/main.py
+++ hash_table/main.py
@@ -1,36 +1,52 @@
+from collections import deque
+import math
+import random
+
 import bucket
 from bucket import Bucket
-from collections import deque
 import hash_table_chains
 from hash_table_chains import ChainsHashTable
-from hash import hash_const
+import hash
+
+
+size = 10 ** 4
+random_range = 5 * 10 ** 4
+
+
+def get_metrics(hash_fn):
+    table = ChainsHashTable(size, hash_fn)
+    for i in range(size):
+        random_key = random.randint(1, random_range)
+        table.set(random_key, random.randint(0, random_range))
+    return {'average-chain-length': get_average_chain_length(table), 'medium-chain-length': get_medium_chain_length(table)}
+
+
+def get_average_chain_length(table):
+    table = table.table
+    elements = 0
+    count = 0
+    for bucket in table:
+        elements += len(bucket)
+        if len(bucket) > 0:
+            count += 1
+    return elements / count
+
+
+def get_medium_chain_length(table):
+    tables = table.table
+    tables.sort(key=lambda bucket: len(bucket))
+    return len(tables[len(tables) // 2])
 
 if __name__ == "__main__":
-    
-    table = ChainsHashTable(hash_function=hash_const)
 
-    table.set(3, 1)
-    table.set(5, 2)
-    table.set(table.size*2 + 3, 3)
-    table.set(table.size + 3, 4)
+    hashes = [
+        {"description": "key % size", "function": hash.prime_hash},
+        {"description": "[size * {key * (sqrt(5) - 1 ) / 2}]", "function": hash.func()},
+        {"description": "[size * {key * (sqrt(3) - 1 ) / 2}]", "function": hash.func(A=(math.sqrt(3) - 1) / 2) },
+        {"description": "[size * {key * (7 - sqrt(45) ) / 2}]", "function": hash.func(A=(7 - math.sqrt(45)) / 2) },
+        {"description": "[size * {key * (7 - sqrt(48) ) / 2}]", "function": hash.func(A=(7 - math.sqrt(48)) / 2) }
+    ]
 
-    """print(table.get(3)[0].value == 5)
-    print(table.get(5)[0].value == 6)
-    print(table.get(table.size*2 + 3)[1].value == 7)
-    print(table.get(table.size + 3)[2][2].value == 8)"""
-
-    print(table.get(3))
-    print(table.get(5))
-    print(table.get(table.size*2 + 3))
-    print(table.get(table.size + 3))
-
-    print(type(table.get(table.size + 3).value))
-
-    print("-------------------")
-
-    for count, x in enumerate(table.table):
-        if len(x) != 0:
-            print("index - {} | {}".format(count, x[0]))
-            print("{}".format(x[0].value))
-
-    
+    for hash in hashes:
+        print(hash["description"])
+        print(get_metrics(hash["function"]))
--- hash_table/test.py
+++ hash_table/test.py
@@ -1,5 +1,7 @@
 import hash_table_chains
+import hash_table_open_addressing
 from hash_table_chains import ChainsHashTable
+from hash_table_open_addressing import OpenAddressingHashTable
 from hash import hash_const, func
 import math
 
@@ -16,10 +18,22 @@ def test_add():
     assert table.table[table.hash_function(3, table.size)][1].value == 7
     assert table.table[table.hash_function(3, table.size)][2].value == 8
 
+    table = OpenAddressingHashTable(size=29)   
+
+    table.set(3, 5)
+    table.set(5, 6)
+    table.set(9, 7)
+    table.set(11, 8)
+
+    print(table.table[table.hash_function(table.size*2 + 3, table.size)])
+    
+    assert table.table[table.hash_function(3, table.size)].value == 5
+    assert table.table[table.hash_function(5, table.size)].value == 6
+    assert table.table[table.hash_function(9, table.size)].value == 7
+    assert table.table[table.hash_function(11, table.size)].value == 8
+
 def test_get():
     table = ChainsHashTable()
-
-    print(table.__getattribute__('hash_function'))
 
     table.set(3, 5)
     table.set(5, 6)
@@ -32,8 +46,6 @@ def test_get():
     assert table.get(table.size + 3).value == 8
 
     table = ChainsHashTable(hash_function=func())
-
-    print(table.__getattribute__('hash_function'))
 
     table.set(3, 5)
     table.set(5, 6)
@@ -48,7 +60,43 @@ def test_get():
     A = (7 - math.sqrt(45)) / 2
     table = ChainsHashTable(hash_function=func(A=A))
 
-    print(table.__getattribute__('hash_function'))
+    table.set(3, 5)
+    table.set(5, 6)
+    table.set(table.size*2 + 3, 7)
+    table.set(table.size + 3, 8)
+
+    assert table.get(3).value == 5
+    assert table.get(5).value == 6
+    assert table.get(table.size*2 + 3).value == 7
+    assert table.get(table.size + 3).value == 8
+
+
+    table = OpenAddressingHashTable()
+
+    table.set(3, 5)
+    table.set(5, 6)
+    table.set(table.size*2 + 3, 7)
+    table.set(table.size + 3, 8)
+
+    assert table.get(3).value == 5
+    assert table.get(5).value == 6
+    assert table.get(table.size*2 + 3).value == 7
+    assert table.get(table.size + 3).value == 8
+
+    table = ChainsHashTable(hash_function=func())
+
+    table.set(3, 5)
+    table.set(5, 6)
+    table.set(table.size*2 + 3, 7)
+    table.set(table.size + 3, 8)
+
+    assert table.get(3).value == 5
+    assert table.get(5).value == 6 
+    assert table.get(table.size*2 + 3).value == 7
+    assert table.get(table.size + 3).value == 8
+
+    A = (7 - math.sqrt(45)) / 2
+    table = ChainsHashTable(hash_function=func(A=A))
 
     table.set(3, 5)
     table.set(5, 6)
