e39a4e5e19ef820338d28c94c921318bc87987bf
volynvlad <volynvlad@gmail.com>
2019-11-04 08:58:07 +0300

    added cases for analysis

--- hash_table/main.py
+++ hash_table/main.py
@@ -1,4 +1,15 @@
-from collections import deque
+"""
+На множестве ключей U |u|=10^k, k={4,5,6,7,........}
+1) подобрать такое число m - простое, A -константа т что
+при n экспериментах на случайных U
+а) h(k) = k % m
+b) h(k) = [m{kA}] - высчитать среднюю длину цепочек и медианную длину цепочек
+а также реализовать хеширование и разрешение коллизии:
+цепочки, открытая адрессация, двойное хеширование
+сравнить с константой кнута (к5-1)/2
+"""
+
+
 import math
 import random
 
@@ -6,19 +17,21 @@ import bucket
 from bucket import Bucket
 import hash_table_chains
 from hash_table_chains import ChainsHashTable
+import hash_table_open_addressing
+from hash_table_open_addressing import OpenAddressingHashTable
 import hash
 
 
-size = 10 ** 4
-random_range = 5 * 10 ** 4
+size = 10 ** 5
+random_range = 5 * size
 
 
-def get_metrics(hash_fn):
-    table = ChainsHashTable(size, hash_fn)
-    for i in range(size):
+def get_metrics(hash_function):
+    table = ChainsHashTable(size, hash_function)
+    for _ in range(size):
         random_key = random.randint(1, random_range)
         table.set(random_key, random.randint(0, random_range))
-    return {'average-chain-length': get_average_chain_length(table), 'medium-chain-length': get_medium_chain_length(table)}
+    return {"average chain length": get_average_chain_length(table), "medium chain length": get_medium_chain_length(table)}
 
 
 def get_average_chain_length(table):
@@ -41,12 +54,24 @@ if __name__ == "__main__":
 
     hashes = [
         {"description": "key % size", "function": hash.prime_hash},
+        # root of x^2 - a * x - 1 = 0
         {"description": "[size * {key * (sqrt(5) - 1 ) / 2}]", "function": hash.func()},
-        {"description": "[size * {key * (sqrt(3) - 1 ) / 2}]", "function": hash.func(A=(math.sqrt(3) - 1) / 2) },
+        {"description": "[size * {key * (sqrt(8) - 2 ) / 2}]", "function": hash.func(A=(math.sqrt(8) - 2) / 2)},
+        {"description": "[size * {key * (sqrt(13) - 3 ) / 2}]", "function": hash.func(A=(math.sqrt(13) - 3) / 2)},
+        {"description": "[size * {key * (sqrt(20) - 4 ) / 2}]", "function": hash.func(A=(math.sqrt(20) - 4) / 2)},
+        {"description": "[size * {key * (sqrt(29) - 5 ) / 2}]", "function": hash.func(A=(math.sqrt(29) - 5) / 2)},
+        {"description": "[size * {key * (sqrt(40) - 6 ) / 2}]", "function": hash.func(A=(math.sqrt(40) - 6) / 2)},
+        {"description": "[size * {key * (sqrt(53) - 7 ) / 2}]", "function": hash.func(A=(math.sqrt(53) - 7) / 2)},
+        # root of x^2 - a * x + 1 = 0
+        {"description": "[size * {key * (3 - sqrt(5) ) / 2}]", "function": hash.func(A=(3 - math.sqrt(5)) / 2) },
+        {"description": "[size * {key * (4 - sqrt(12) ) / 2}]", "function": hash.func(A=(4 - math.sqrt(12)) / 2) },
+        {"description": "[size * {key * (5 - sqrt(21) ) / 2}]", "function": hash.func(A=(5 - math.sqrt(21)) / 2) },
+        {"description": "[size * {key * (6 - sqrt(32) ) / 2}]", "function": hash.func(A=(6 - math.sqrt(32)) / 2) },
         {"description": "[size * {key * (7 - sqrt(45) ) / 2}]", "function": hash.func(A=(7 - math.sqrt(45)) / 2) },
-        {"description": "[size * {key * (7 - sqrt(48) ) / 2}]", "function": hash.func(A=(7 - math.sqrt(48)) / 2) }
+        {"description": "[size * {key * (8 - sqrt(60) ) / 2}]", "function": hash.func(A=(8 - math.sqrt(60)) / 2) },
+        {"description": "[size * {key * (9 - sqrt(77) ) / 2}]", "function": hash.func(A=(9 - math.sqrt(77)) / 2) },
     ]
 
     for hash in hashes:
-        print(hash["description"])
+        print(hash["description"].ljust(36, ), end=" ")
         print(get_metrics(hash["function"]))
