c5a39804333199f4800ee79a828ecb0774d9b90a
volynvlad <volynvlad@gmail.com>
2020-02-25 10:07:52 +0300

    add docstring

--- turing_machine/test.py
+++ turing_machine/test.py
@@ -1,22 +1,30 @@
 from algorithms_python.turing_machine.turing_machine import TuringMachine
 
-matrix_ = [[("qy", "a", 0), ("q1", "a", 0), ("q1", "b", 0), ("q1", "c", 0)],
-           [("qy", "a", 0), ("q2", "#", 1), ("q2", "#", 1), ("q2", "#", 1)],
-           [("qy", "#", 0), ("q1", "#", 1), ("q1", "#", 1), ("q1", "#", 1)]]
-dictionary_ = ["a", "b", "c"]
-states_ = ["q0", "q1", "q2"]
+ft_matrix = [[("qy", "a", 0), ("q1", "a", 0), ("q1", "b", 0), ("q1", "c", 0)],
+             [("qy", "a", 0), ("q2", "#", 1), ("q2", "#", 1), ("q2", "#", 1)],
+             [("qy", "#", 0), ("q1", "#", 1), ("q1", "#", 1), ("q1", "#", 1)]]
+ft_dictionary = ["a", "b", "c"]
+ft_states = ["q0", "q1", "q2"]
 
-turing_machine = TuringMachine(dictionary_, states_, matrix_)
+first_task = TuringMachine(ft_dictionary, ft_states, ft_matrix)
 
 
 def test_even_case():
-    assert turing_machine.start("abcabc").count("a") == 1
+    """
+    test for even case for the first task
+    """
+    assert first_task.start("abcabc").count("a") == 1
 
 
 def test_odd_case():
-    assert turing_machine.start("abc").count("a") == 0
+    """
+    test for odd case for the first task
+    """
+    assert first_task.start("abc").count("a") == 0
 
 
 def test_empty_case():
-    assert turing_machine.start("").count("a") == 1
-
+    """
+    test for empty case for the first task
+    """
+    assert first_task.start("").count("a") == 1
--- turing_machine/turing_machine.py
+++ turing_machine/turing_machine.py
@@ -1,31 +1,47 @@
+"""
+turing_machine module
+"""
 import pandas as pd
 
 
 class TuringMachine:
+    """
+    TuringMachine
+    """
     def __init__(self, dictionary, states, matrix):
         self.dictionary = dictionary
         self.states = states
         self.matrix = matrix
-        self.table = pd.DataFrame(matrix, columns=["#"] + dictionary, index=states)
+        self.table = pd.DataFrame(matrix, columns=["#"] + dictionary,
+                                  index=states)
         self.tape = []
 
-    def display(self, tape):
+    @staticmethod
+    def display(tape):
+        """
+        display tape as a string in the console
+        """
         print(''.join(tape))
 
     def run(self, state, position):
-        if state == "qy" or state == "qn":
+        """
+        run method
+        """
+        if state in ('qy', 'qn'):
             return
-        next_state, letter, move_position = self.table[self.tape[position]][state]
+        next_state, letter, move_position = \
+            self.table[self.tape[position]][state]
         self.tape[position] = letter
         position += move_position
         self.run(next_state, position)
 
     def start(self, word):
+        """
+        start method
+        """
         head_position = 0
         current_state = "q0"
         self.tape = [c for c in word if c in self.dictionary] + ["#"] * 5
 
         self.run(current_state, head_position)
         return ''.join(self.tape)
-
-
