d481f6223749268a2d0faf28c13cd4191b392527
volynvlad <volynvlad@gmail.com>
2020-05-15 13:53:04 +0300

    added results

--- local_minimum/main.py
+++ local_minimum/main.py
@@ -1,4 +1,5 @@
 import random
+import time
 
 from local_minimum.tsp import tsp
 from graph.matrix_graph import GraphAdjMatrix
@@ -11,10 +12,21 @@ if __name__ == "__main__":
 
     graph = GraphAdjMatrix(matrix, number_nodes)
 
-    for i in range(number_nodes):
-        for j in range(i + 1, number_nodes):
-            graph.add_double_edge((i, j), weight=random.randint(*random_range))
+    sum_l = 0
+    iter_num = 40
+    coef = 6
 
-    local_minimum_tsp = tsp(graph)
+    start_end = 0
+    for k in range(iter_num):
+        for i in range(number_nodes):
+            for j in range(i + 1, number_nodes):
+                graph.add_double_edge((i, j), weight=random.randint(*random_range))
+        start = time.time()
+        local_minimum_tsp = tsp(graph, coef)
+        end = time.time()
+        start_end = start_end + end - start
+        sum_l += graph.get_cycle_weight(local_minimum_tsp)
 
-
+    print(start_end)
+    with open('results', 'a+') as f:
+        f.write(f"mean_weight = {sum_l / iter_num} number_nodes = {number_nodes} coef = {coef} mean_time = {(start_end) / iter_num}\n")
--- local_minimum/tsp.py
+++ local_minimum/tsp.py
@@ -1,10 +1,7 @@
-import random
-
-from graph.list_graph import GraphAdjList
 from graph.matrix_graph import GraphAdjMatrix
 
 
-def tsp(input_graph: GraphAdjMatrix):
+def tsp(input_graph: GraphAdjMatrix, coef):
     if input_graph.size <= 3:
         return list(range(input_graph.size))
     curr_solution = list(range(input_graph.size))
@@ -13,8 +10,8 @@ def tsp(input_graph: GraphAdjMatrix):
     best_neighbor = curr_solution
     best_neighbor_weight = curr_solution_weight
     while True:
-        print(f"curr_solution = {curr_solution}, weight = {curr_solution_weight}")
-        for i in range(0, input_graph.size // 2, 2):
+        # print(f"curr_solution = {curr_solution}, weight = {curr_solution_weight}")
+        for i in range(0, input_graph.size, coef):
             neighbor_sol = curr_solution.copy()
             neighbor_sol[i], neighbor_sol[i + 1] = neighbor_sol[i + 1], neighbor_sol[i]
 
