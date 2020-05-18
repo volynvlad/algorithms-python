4c2402f4d7ee1cb2bc1f344db1cdb56cdf44baa8
volynvlad <volynvlad@gmail.com>
2020-05-18 19:46:50 +0300

    changed algorithm and the way to calculate results

--- local_minimum/main.py
+++ local_minimum/main.py
@@ -7,26 +7,29 @@ from graph.matrix_graph import GraphAdjMatrix
 
 if __name__ == "__main__":
     number_nodes = 500
-    random_range = [1, 10]
+    random_range = [0, number_nodes]
+    random.seed = 42
     matrix = [[0 for x in range(number_nodes)] for y in range(number_nodes)]
 
     graph = GraphAdjMatrix(matrix, number_nodes)
 
-    sum_l = 0
-    iter_num = 40
-    coef = 6
+    ratio = 0
+    iter_num = 16
 
     start_end = 0
+    graph_weight = 0
     for k in range(iter_num):
         for i in range(number_nodes):
+            graph.add_double_edge((i, i), weight=0)
             for j in range(i + 1, number_nodes):
                 graph.add_double_edge((i, j), weight=random.randint(*random_range))
+
         start = time.time()
-        local_minimum_tsp = tsp(graph, coef)
+        local_minimum_tsp = tsp(graph)
         end = time.time()
         start_end = start_end + end - start
-        sum_l += graph.get_cycle_weight(local_minimum_tsp)
+        ratio = ratio + graph.get_cycle_weight(local_minimum_tsp) / graph.get_graph_weight()
 
     print(start_end)
     with open('results', 'a+') as f:
-        f.write(f"mean_weight = {sum_l / iter_num} number_nodes = {number_nodes} coef = {coef} mean_time = {(start_end) / iter_num}\n")
+        f.write(f"mean_ration = {ratio / iter_num} number_nodes = {number_nodes} mean_time = {start_end / iter_num}\n")
--- local_minimum/tsp.py
+++ local_minimum/tsp.py
@@ -1,7 +1,7 @@
 from graph.matrix_graph import GraphAdjMatrix
 
 
-def tsp(input_graph: GraphAdjMatrix, coef):
+def tsp(input_graph: GraphAdjMatrix):
     if input_graph.size <= 3:
         return list(range(input_graph.size))
     curr_solution = list(range(input_graph.size))
@@ -9,22 +9,31 @@ def tsp(input_graph: GraphAdjMatrix, coef):
 
     best_neighbor = curr_solution
     best_neighbor_weight = curr_solution_weight
-    while True:
-        # print(f"curr_solution = {curr_solution}, weight = {curr_solution_weight}")
-        for i in range(0, input_graph.size, coef):
-            neighbor_sol = curr_solution.copy()
-            neighbor_sol[i], neighbor_sol[i + 1] = neighbor_sol[i + 1], neighbor_sol[i]
 
-            neighbor_sol_weight = input_graph.get_cycle_weight(neighbor_sol)
+    while True:
+        j = 0
+        for i in range(j + 2, input_graph.size + j - 1):
+            # neighbor_sol = curr_solution.copy()
+            # neighbor_sol[j], neighbor_sol[i % input_graph.size] = neighbor_sol[i % input_graph.size], neighbor_sol[j]
+
+            # swap to get neighbor
+            curr_solution[j], curr_solution[i % input_graph.size] = curr_solution[i % input_graph.size], curr_solution[j]
+
+            neighbor_sol_weight = input_graph.get_cycle_weight(curr_solution)
             best_neighbor_weight = input_graph.get_cycle_weight(best_neighbor)
             if neighbor_sol_weight < best_neighbor_weight:
-                best_neighbor = neighbor_sol
+                best_neighbor = curr_solution.copy()
                 best_neighbor_weight = neighbor_sol_weight
 
+            # swap again to get initial solution
+            curr_solution[j], curr_solution[i % input_graph.size] = curr_solution[i % input_graph.size], curr_solution[j]
+
+        # update current solution
         if best_neighbor_weight < curr_solution_weight:
-            curr_solution = best_neighbor
+            curr_solution = best_neighbor.copy()
             curr_solution_weight = best_neighbor_weight
         else:
             break
+        j += 1
 
     return curr_solution
