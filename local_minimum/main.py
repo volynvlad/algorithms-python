d4bf45462fcde36348469ec16f3fedd4528365ba
volynvlad <volynvlad@gmail.com>
2020-05-27 15:10:45 +0300

    changed main

--- local_minimum/main.py
+++ local_minimum/main.py
@@ -6,30 +6,43 @@ from graph.matrix_graph import GraphAdjMatrix
 
 
 if __name__ == "__main__":
-    number_nodes = 100
-    random_range = [0, number_nodes]
-    random.seed = 42
+    # number_nodes = 100
+    # random_range = [0, number_nodes]
+    # random.seed = 42
+    # matrix = [[0 for x in range(number_nodes)] for y in range(number_nodes)]
+    #
+    # graph = GraphAdjMatrix(matrix, number_nodes)
+    #
+    # graph_weight = 0
+    # for i in range(number_nodes):
+    #     graph.add_double_edge((i, i), weight=0)
+    #     for j in range(i + 1, number_nodes):
+    #         graph.add_double_edge((i, j), weight=random.randint(*random_range))
+    #
+    #     local_minimum_tsp = tsp(graph)
+    #     print(graph.get_cycle_weight(local_minimum_tsp))
+    #     print(graph.get_graph_weight())
+
+    number_nodes = 10
     matrix = [[0 for x in range(number_nodes)] for y in range(number_nodes)]
 
     graph = GraphAdjMatrix(matrix, number_nodes)
 
-    ratio = 0
-    iter_num = 16
+    for i in range(number_nodes):
+        for j in range(i + 1, number_nodes):
+            graph.add_double_edge((i, j), weight=10)
 
-    start_end = 0
-    graph_weight = 0
-    for k in range(iter_num):
-        for i in range(number_nodes):
-            graph.add_double_edge((i, i), weight=0)
-            for j in range(i + 1, number_nodes):
-                graph.add_double_edge((i, j), weight=random.randint(*random_range))
+    local_min_tsp = tsp(graph)
 
-        start = time.time()
-        local_minimum_tsp = tsp(graph)
-        end = time.time()
-        start_end = start_end + end - start
-        ratio = ratio + graph.get_cycle_weight(local_minimum_tsp) / graph.get_graph_weight()
+    print(f"solution = {local_min_tsp}")
+    print(f"weight = {graph.get_cycle_weight(local_min_tsp)}")
+    print('-' * 30)
 
-    print(start_end)
-    with open('results', 'a+') as f:
-        f.write(f"mean_ration = {ratio / iter_num} number_nodes = {number_nodes} mean_time = {start_end / iter_num}\n")
+    cycle = [2, 4, 6, 8, 0, 1, 3, 5, 7, 9]
+    for i in range(len(cycle)):
+        graph.add_double_edge((cycle[i - 1], cycle[i]), weight=1)
+
+    local_min_tsp = tsp(graph)
+
+    print(f"solution = {local_min_tsp}")
+    print(f"weight = {graph.get_cycle_weight(local_min_tsp)}")
